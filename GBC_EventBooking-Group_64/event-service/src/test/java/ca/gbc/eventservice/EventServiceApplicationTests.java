69d13139489df3d12fe3ce5baf11179913b6222d
Simon K <Simon@DESKTOP-R9UCS0H>
2024-11-10 11:12:07 -0800

    event-service updates and testing

--- GBC_EventBooking-Group_64/event-service/src/test/java/ca/gbc/eventservice/EventServiceApplicationTests.java
+++ GBC_EventBooking-Group_64/event-service/src/test/java/ca/gbc/eventservice/EventServiceApplicationTests.java
@@ -1,15 +1,69 @@
 package ca.gbc.eventservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+
+// @Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EventServiceApplicationTests {
 
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+
+    @LocalServerPort
+    private Integer port;
+
+    //  http://localhost:port/api/event
+    @BeforeEach
+    void setup(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+
     @Test
-    void contextLoads() {
+    void createEventTest() {
+
+        String requestBody = """
+                {
+                   "eventName" : "Study Hall",
+                   "eventType" : "study",
+                   "expectedAttendees" : "2",
+                   "organizerId" : "001"
+                }
+        """;
+
+        // BDD - Behavioural Driven Development (Given, When, Then)
+        // Rest Assured is used to perform HTTP requests and verify responses.
+        // This test performs a POST request to the /api/product endpoint to create a new product.
+        // Then it verifies that the response status is 201 (Created) and the response body contains the correct product details.
+        RestAssured.given()
+                .contentType("application/json")  //  Set the content type of the request to JSON
+                .body(requestBody)  //  Pass the request body (the product data)
+                .when()
+                .post("/api/event")  //  Perform the POST request to the /api/product endpoint
+                .then()
+                .log().all()  //  Log the response details
+                .statusCode(201)  // Assert that the HTTP status code is 201 Created
+                .body("id", Matchers.notNullValue())  //  Assert that the returned product has a non-null ID
+                .body("eventName", Matchers.equalTo("Study Hall"))  //  Assert that the product's name matches
+                .body("eventType", Matchers.equalTo("study"))  //  Assert that the description matches
+                .body("organizerId", Matchers.equalTo(001));  //  Assert that the price is 2000
+
+
     }
 
 }
