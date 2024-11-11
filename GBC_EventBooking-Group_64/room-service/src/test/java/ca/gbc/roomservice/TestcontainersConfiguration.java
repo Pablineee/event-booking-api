2193572992527d7e4d56f4767aef19e0b4d60886
System Administrator <110475574+Pablineee@users.noreply.github.com>
2024-11-11 17:17:19 -0500

    Implemented integration testing for room-service

--- GBC_EventBooking-Group_64/room-service/src/test/java/ca/gbc/roomservice/TestcontainersConfiguration.java
+++ GBC_EventBooking-Group_64/room-service/src/test/java/ca/gbc/roomservice/TestcontainersConfiguration.java
@@ -1,18 +1,27 @@
 package ca.gbc.roomservice;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.wait.strategy.Wait;
+//import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@EnableJpaRepositories(basePackages = "ca.gbc.roomservice.repository")
+@EntityScan(basePackages = "ca.gbc.roomservice.model")
 class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    public PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:15-alpine")
+                .withDatabaseName("room-service")
+                .withUsername("admin")
+                .withPassword("password")
+                .waitingFor(Wait.forListeningPort());
     }
 
 }
