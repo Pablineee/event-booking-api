8c883ce4ae453ec5a973704606e04fb6f7af6619
System Administrator <110475574+Pablineee@users.noreply.github.com>
2024-12-07 23:45:32 -0500

    Add api-gateway Postman collection, as well as assignment report

--- GBC_EventBooking-Group_64/approval-service/src/main/java/ca/gbc/approvalservice/client/UserClient.java
+++ GBC_EventBooking-Group_64/approval-service/src/main/java/ca/gbc/approvalservice/client/UserClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "http://user-service:9003", path = "api/user")
+@FeignClient(name = "user-service", url = "http://user-service:8080", path = "api/user")
 public interface UserClient {
     @GetMapping("/is-staff/{userId}")
     boolean isStaff(@PathVariable("userId") Long userId);
